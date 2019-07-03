6835322318efaaba672d43f656393285bcd1bad8
rasanjaya85 <rlsubasinghe85@gmail.com>
2019-07-03 18:55:02 +0530

    Added the celcius values to microservice

--- src/main/java/org/wso2/weather/WeatherService.java
+++ src/main/java/org/wso2/weather/WeatherService.java
@@ -106,7 +106,7 @@ public class WeatherService extends Microservice{
         @GET
         @Produces("application/json")
         public Map<String, String>  getResources() {
-             return ImmutableMap.of("weather-location",config.getString("weather.location"),"microservice-name",config.getString("microservice.name"));
+             return ImmutableMap.of("microservice-name",config.getString("microservice.name"),"weather-location",config.getString("weather.location"),"weather (˚C)",config.getString("weather.celcius"));
         }
     }
 
